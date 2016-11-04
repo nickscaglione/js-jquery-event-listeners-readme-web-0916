6d5e82d7190c1a07bf2dc5e792fda4898f69f343
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-11-04 15:33:46 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  var images = $('img')
+  images.on('load', function() {
+    images.addClass("tasty")
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+    if (key === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 });
